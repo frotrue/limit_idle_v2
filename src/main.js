ab7aeafe72b6802d54c901ef17cb5e5a7fef1753
Frotrue <105928069+frotrue@users.noreply.github.com>
2026-06-11 14:23:40 +0900

    refactor: import stability patches from game persistence

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { applyPreLoadSavePatches, applyRuntimeStabilityPatches } from './stabilityPatches.js'
+import { applyPreLoadSavePatches, applyRuntimeStabilityPatches } from './game/persistence/stabilityPatches.js'
 
 applyPreLoadSavePatches()
 
