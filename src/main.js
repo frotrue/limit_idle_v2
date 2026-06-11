064e3d299291cd5a66c390c20b16d3b4e856b36c
Frotrue <105928069+frotrue@users.noreply.github.com>
2026-06-11 15:05:35 +0900

    perf: install Decimal pow cache at startup

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { installDecimalPowCache } from './game/performance/decimalPowCache.js'
 import { applyPreLoadSavePatches, applyRuntimeStabilityPatches } from './game/persistence/stabilityPatches.js'
 
+installDecimalPowCache()
 applyPreLoadSavePatches()
 
 createApp(App).mount('#app')
