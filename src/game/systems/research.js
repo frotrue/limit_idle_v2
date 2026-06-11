2d7f835b549eac4b691f4f59f18e04567e8d5505
Frotrue <105928069+frotrue@users.noreply.github.com>
2026-06-11 14:22:05 +0900

    refactor: add research system facade

---
+++ src/game/systems/research.js
@@ -0,0 +1,13 @@
+export {
+  purchaseResearch,
+  getResearchState,
+  AP_RESEARCH_NODES
+} from '../../gameLogic.js';
+
+export {
+  canPurchaseResearch,
+  isAutoResearched,
+  hasAnyAutoResearch,
+  getResearchBonuses,
+  AUTO_RESEARCH_MAP
+} from '../../apResearch.js';
