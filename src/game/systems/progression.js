7bf908bf17f9293dc90f9159b07617135610a41c
Frotrue <105928069+frotrue@users.noreply.github.com>
2026-06-11 14:21:59 +0900

    refactor: add progression system facade

---
+++ src/game/systems/progression.js
@@ -0,0 +1,12 @@
+export {
+  makefx,
+  equation_calc,
+  differentiate,
+  integrate_calc,
+  manualTick,
+  getIntegralBonusValue,
+  getTier2MilestoneState,
+  getTier2MilestoneTable,
+  getTier3MilestoneState,
+  getTier3MilestoneTable
+} from '../../gameLogic.js';
